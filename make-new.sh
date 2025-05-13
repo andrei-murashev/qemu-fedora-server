ff6cdbd7b025a2fcfd2711477ddfe054acfd2dcc
Andrei Murashev <andrei.murashev.1@gmail.com>
2025-05-13 20:09:59 +1000

    `make-new.sh` improvements
    Script checks with the user if it is okay to download the latest QCOW image. QCOW image is resized to 20G by default, but this can be changed easily. The QCOW link is always acquired now, because before sometimes `chromium` would not start properly. If this happens, it is just restarted to try again.

--- make-new.sh
+++ make-new.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-QCOW_LINK=$(
-  chromium-browser --headless --disable-gpu --dump-dom                \
-  --password-store=basic 'https://fedoraproject.org/server/download'  \
-  | sed -e 's/>/>\n/g'                                                \
-  | grep 'Fedora-Server-Guest-Generic'                                \
-  | grep 'x86_64'                                                     \
-  | grep -oP '"\K[^"]+(?=")'                                          \
-  | head -n1
-)
+while [[ "$QCOW_LINK" == '' ]]; do
+  QCOW_LINK=$(
+    chromium-browser --headless --disable-gpu --dump-dom                \
+    --password-store=basic 'https://fedoraproject.org/server/download'  \
+    | sed -e 's/>/>\n/g'                                                \
+    | grep 'Fedora-Server-Guest-Generic'                                \
+    | grep 'x86_64'                                                     \
+    | grep -oP '"\K[^"]+(?=")'                                          \
+    | head -n1
+  )
+done
 
 
 CHECKSUM_LINK="\
@@ -22,13 +24,22 @@ $(echo "$QCOW_LINK" \
 )-CHECKSUM"
 
 
-# curl -SLO "$CHECKSUM_LINK"
+curl -sSLO "$CHECKSUM_LINK"
 CHECKSUM_FILE=$(echo "$CHECKSUM_LINK" | sed -e 's!.*/!!')
 
 sha256sum -c "$CHECKSUM_FILE" 2>/dev/null
 CHECKSUM_STATUS="$?"
 
 if [[ "$CHECKSUM_STATUS" != 0 ]]; then
-  echo "SUM CHECK FAILED: Downloading latest server image."
-  # curl -SLO "$QCOW_LINK"
+  read -p "SUM CHECK FAILED: Download latest image? [y/N]: " ans
+
+  if [[ "$ans" == 'y' || "$ans" == 'Y' ]]; then
+    curl -SLO "$QCOW_LINK"
+  else
+    exit 0
+  fi
 fi
+
+
+QCOW_FILE=$(ls -h | grep *'.qcow2' | grep 'Fedora-Server')
+qemu-img resize "$QCOW_FILE" 20G
