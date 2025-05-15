2371b9adafdc8646b9176b6c37bcb85eda1222fe
Andrei Murashev <andrei.murashev.1@gmail.com>
2025-05-15 12:54:39 +1000

    `make-new.sh` refinements
    Cleaned up calls to UNIX utilities (`sed` and `grep`), added automatic backing up of the QCOW image if it shares the same name as the image to be downloaded. Images are compared by their checksums (in case I have not already mentioned).

--- make-new.sh
+++ make-new.sh
@@ -1,45 +1,70 @@
 #!/bin/bash
 
-while [[ "$QCOW_LINK" == '' ]]; do
+# ----------------------------------------------------------
+# HELPER FUNCTIONS
+# ----------------------------------------------------------
+
+function backup_file {
+  [[ -z $1 ]] && return 1
+
+  readarray -t USED_HEXES < <(ls -a | grep -oP '.bak-[0-9-aA-fF]{16}$')
+
+  while [[ *"$HEX"* == "${USED_HEXES[*]}" || -z "$HEX" ]]; do
+    HEX=$(openssl rand -hex 8)
+  done
+
+  mv -v "$1" "$1.bak-$HEX"
+}
+
+# ----------------------------------------------------------
+
+
+
+while [[ -z "$QCOW_LINK" ]]; do
   QCOW_LINK=$(
     chromium-browser --headless --disable-gpu --dump-dom                \
     --password-store=basic 'https://fedoraproject.org/server/download'  \
-    | sed -e 's/>/>\n/g'                                                \
-    | grep 'Fedora-Server-Guest-Generic'                                \
-    | grep 'x86_64'                                                     \
+    | sed  's/>/>\n/g'                                                  \
+    | grep -e 'Fedora-Server-Guest-Generic' -e 'x86_64'                 \
     | grep -oP '"\K[^"]+(?=")'                                          \
     | head -n1
   )
 done
 
-
 CHECKSUM_LINK="\
 https://ap.edge.kernel.org\
 $(echo "$QCOW_LINK" \
-  | sed -e 's/.qcow2$//'                \
-  | sed -e 's/-Guest-Generic//g'        \
-  | sed -e 's/.*\/fedora\//\/fedora\//' \
-  | sed -e 's/\/linux//'                \
-  | sed 's/\(.*\)\./\1-/'
+  | sed \
+  -e 's/.qcow2$//'                \
+  -e 's/-Guest-Generic//g'        \
+  -e 's/.*\/fedora\//\/fedora\//' \
+  -e 's/\/linux//'                \
+  -e 's/\(.*\)\./\1-/'
 )-CHECKSUM"
 
 
 curl -sSLO "$CHECKSUM_LINK"
-CHECKSUM_FILE=$(echo "$CHECKSUM_LINK" | sed -e 's!.*/!!')
+CHECKSUM_FILE=$(echo "$CHECKSUM_LINK" | sed 's!.*/!!')
 
-sha256sum -c "$CHECKSUM_FILE" 2>/dev/null
+CHECKSUM_EXPECTED=$(cat "$CHECKSUM_FILE" | grep '^SHA256' | awk '{print $NF}')
+CHECKSUM_CURRENT=$(sha256sum 'Fedora-Server'*'.qcow2' 2> /dev/null \
+  | awk '{print $1}' || echo 0)
+
+[[ "$CHECKSUM_CURRENT" == "$CHECKSUM_EXPECTED" ]]
 CHECKSUM_STATUS="$?"
 
 if [[ "$CHECKSUM_STATUS" != 0 ]]; then
-  read -p "SUM CHECK FAILED: Download latest image? [y/N]: " ans
+  read -p "Download latest image? [y/N]: " ans
 
-  if [[ "$ans" == 'y' || "$ans" == 'Y' ]]; then
+  if [[ "${ans^^}" == 'Y' || "${ans^^}" == 'YES' ]]; then
+    QCOW_FILE=$(ls -a | grep '.qcow2$' | grep 'Fedora-Server')
+    backup_file "$QCOW_FILE" || true
     curl -SLO "$QCOW_LINK"
+
   else
     exit 0
   fi
+
+else
+  echo "A clean image already exists in this directory."
 fi
-
-
-QCOW_FILE=$(ls -h | grep *'.qcow2' | grep 'Fedora-Server')
-qemu-img resize "$QCOW_FILE" 20G
